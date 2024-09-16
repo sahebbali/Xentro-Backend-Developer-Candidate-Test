177b342a88ba7edcd65bcc8aaf4aa47b910bc4c6
sahebbali <saheb1996jmr@gmail.com>
2024-09-16 11:37:09 +0600

    fixed RBAC issue

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const { auth, role } = require("../middleware/authMiddleware");
 
 router.get("/products", productController.getAllProducts);
 router.get("/products/:id", productController.getProductById);
-router.post("/products", productController.createProduct);
-router.put("/products/:id", productController.updateProduct);
-router.delete("/products/:id", productController.deleteProduct);
+router.post(
+  "/products",
+  auth,
+  role(["Admin"]),
+  productController.createProduct
+);
+router.put(
+  "/products/:id",
+  auth,
+  role(["Admin"]),
+  productController.updateProduct
+);
+router.delete(
+  "/products/:id",
+  auth,
+  role(["Admin"]),
+  productController.deleteProduct
+);
 
 module.exports = router;
